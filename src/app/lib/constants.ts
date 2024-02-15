941f2d4daf79725e1036526aebb8a63c20cfd16f
Justin Clark <jclarksummit@gmail.com>
2024-02-15 17:27:06 -0500

    Add const for NAV_LINKS

---
+++ src/app/lib/constants.ts
@@ -0,0 +1,18 @@
+export const NAV_LINKS = [
+  {
+    title: "About",
+    path: "#about",
+  },
+  {
+    title: "Projects",
+    path: "#projects",
+  },
+  {
+    title: "Resume",
+    path: "/",
+  },
+  {
+    title: "Contact",
+    path: "#contact",
+  },
+]
