fcbe4944d550adbcd8c54f4f4ae5a1ef12305c72
Justin Clark <jclarksummit@gmail.com>
2024-02-15 17:26:36 -0500

    Add NavBar to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#161b22]">
-      <div className=" container mx-auto px-12 p-4">
+      <NavBar />
+      <div className=" container mx-auto px-12 p-4 mt-24">
         <HeroSection />
       </div>
     </main>
