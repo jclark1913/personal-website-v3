74df41f438450010f9676fe092dab020c5a0246f
Justin Clark <jclarksummit@gmail.com>
2024-02-15 20:07:39 -0500

    Add component to dynamically gerneate tabs

---
+++ src/app/components/TabButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface TabButtonProps {
+  active: boolean;
+  selectActiveTab: () => void;
+  children: React.ReactNode;
+}
+
+const TabButton:React.FC<TabButtonProps> = ({ active, selectActiveTab, children }) => {
+    const buttonStyling = active ? "text-white border-b border-blue-500 " : "text-slate-200";
+  return (
+    <button onClick={selectActiveTab}>
+      <p className={`mr-3 font-semibold hover:text-white ${buttonStyling}`}>{children}</p>
+    </button>
+  );
+};
+
+export default TabButton;
