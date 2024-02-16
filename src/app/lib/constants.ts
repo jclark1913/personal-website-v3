266958f5481f93945172a1d34745a7d616f6c404
Justin Clark <jclarksummit@gmail.com>
2024-02-15 20:06:41 -0500

    Add consts for work/education experience

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -15,4 +15,83 @@ export const NAV_LINKS = [
     title: "Contact",
     path: "#contact",
   },
+];
+
+export const WORK_EXPERIENCE = [
+  {
+    title: "Software Engineer",
+    institution: "Rootwise, LLC",
+    date: "2024 - Present",
+    info: null,
+  },
+  {
+    title: "Software Consultant",
+    institution: "ETANA Syria",
+    date: "2023 - 2024",
+    info: null,
+  },
+  {
+    title: "Software Developer Intern",
+    institution: "Rithm School",
+    date: "2023",
+    info: null,
+  },
+  {
+    title: "Senior Analyst",
+    institution: "ETANA Syria",
+    date: "2019 - 2023",
+    info: null,
+  },
+  {
+    title: "Journalist and Translator",
+    institution: "Syria Direct, Freelance",
+    date: "2016 - 2019",
+    info: null,
+  },
+  {
+    title: "Arabic Interpreter",
+    institution: "Health Point Foundation",
+    date: "2015",
+    info: null,
+  }
+];
+
+export const EDUCATION = [
+  {
+    title: "Full Stack Web Development",
+    institution: "Rithm School",
+    date: "2023",
+    info: "Remote",
+  },
+  {
+    title: "Arabic Language",
+    institution: "Western Michigan University",
+    date: "2013-2016",
+    info: "Kalamazoo, MI",
+  },
+  {
+    title: "Arabic Language",
+    institution: "Qasid Institute",
+    date: "2015-2016",
+    info: "Amman, Jordan",
+  },
+  {
+    title: "Arabic Language",
+    institution: "Bethlehem University",
+    date: "2015",
+    info: "Bethlehem, Palestine",
+  }
 ]
+
+export const ABOUT_TAB_DATA = [
+  {
+    title: "Work Experience",
+    id: "work",
+    content: WORK_EXPERIENCE
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: EDUCATION
+  }
+];
