48cca14b28c7c0fd8cd8769b2dc5781d4f968503
Justin Clark <jclarksummit@gmail.com>
2024-02-16 16:30:17 -0500

    Format file

--- src/app/components/TabButton.tsx
+++ src/app/components/TabButton.tsx
@@ -6,11 +6,19 @@ interface TabButtonProps {
   children: React.ReactNode;
 }
 
-const TabButton:React.FC<TabButtonProps> = ({ active, selectActiveTab, children }) => {
-    const buttonStyling = active ? "text-white border-b border-blue-500 " : "text-slate-200";
+const TabButton: React.FC<TabButtonProps> = ({
+  active,
+  selectActiveTab,
+  children,
+}) => {
+  const buttonStyling = active
+    ? "text-white border-b border-blue-500 "
+    : "text-slate-200";
   return (
     <button onClick={selectActiveTab}>
-      <p className={`mr-3 font-semibold hover:text-white ${buttonStyling}`}>{children}</p>
+      <p className={`mr-3 font-semibold hover:text-white ${buttonStyling}`}>
+        {children}
+      </p>
     </button>
   );
 };
