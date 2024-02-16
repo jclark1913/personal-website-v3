e006fdb7ba24b1750ee282805e2c53747b1b552a
Justin Clark <jclarksummit@gmail.com>
2024-02-16 16:30:01 -0500

    Format file

--- src/app/components/SkillsSection.tsx
+++ src/app/components/SkillsSection.tsx
@@ -7,9 +7,14 @@ const SkillsSection = () => {
       <h2 className="text-center text-md sm:text-lg">My tech stack:</h2>
       <div className="grid gap-4 mt-4 grid-cols-4 md:grid-cols-6 lg:grid-cols-8 sm:w-1/2 sm:mx-auto">
         {TECHNOLOGIES.map((tech) => (
-          <div key={tech.title} className="flex flex-col items-center gap-1 relative group hover:cursor-pointer">
+          <div
+            key={tech.title}
+            className="flex flex-col items-center gap-1 relative group hover:cursor-pointer"
+          >
             <tech.icon className="size-10 sm:size-12" color={tech.color} />
-            <p className="hidden group-hover:block absolute text-black bg-white text-sm px-2 py-1 rounded-md shadow-lg -translate-y-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300">{tech.title}</p>
+            <p className="hidden group-hover:block absolute text-black bg-white text-sm px-2 py-1 rounded-md shadow-lg -translate-y-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              {tech.title}
+            </p>
           </div>
         ))}
       </div>
