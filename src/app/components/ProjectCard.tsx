f82853ede63899c293804e78e3f57e582b9fff11
Justin Clark <jclarksummit@gmail.com>
2024-02-16 16:29:23 -0500

    Fix bug where background kept getting distorted

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -30,7 +30,7 @@ const ProjectCard: React.FC<ProjectCardsProps> = ({
     <div className="flex flex-col">
       <div
         className="h-52 md:h-72 rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
+        style={{ backgroundImage: `url(${imgUrl})`, backgroundSize: "cover" }}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-1000">
           {repoUrl ? (
