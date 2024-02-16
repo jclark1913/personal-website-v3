0472ce9ad2b3625c4da3e08d66fcd901376bfa4c
Justin Clark <jclarksummit@gmail.com>
2024-02-16 11:04:43 -0500

    Add tech stack data

---
+++ src/app/lib/TechStack.tsx
@@ -0,0 +1,87 @@
+import * as Icons from 'react-icons/si';
+
+export const TECHNOLOGIES = [
+    {
+        title: "Javascript",
+        icon: Icons.SiJavascript,
+        color: "#f7df1e"
+    },
+    {
+        title: "Typescript",
+        icon: Icons.SiTypescript,
+        color: "#007acc"
+    },
+    {
+        title: "React",
+        icon: Icons.SiReact,
+        color: "#61dafb"
+    },
+    {
+        title: "Tailwind",
+        icon: Icons.SiTailwindcss,
+        color: "#38b2ac"
+    },
+    {
+        title: "Node.js",
+        icon: Icons.SiNodedotjs,
+        color: "#68a063"
+    },
+    {
+        title: "Express",
+        icon: Icons.SiExpress,
+        color: "#000000"
+    },
+    {
+        title: "PostgreSQL",
+        icon: Icons.SiPostgresql,
+        color: "#336791"
+    },
+    {
+        title: "Python",
+        icon: Icons.SiPython,
+        color: "#306998"
+    },
+    {
+        title: "Django",
+        icon: Icons.SiDjango,
+        color: "#092e20"
+    },
+    {
+        title: "AWS",
+        icon: Icons.SiAmazonaws,
+        color: "#232f3e"
+    },
+    {
+        title: "Flask",
+        icon: Icons.SiFlask,
+        color: "#000000"
+    },
+    {
+        title: "NextJS",
+        icon: Icons.SiNextdotjs,
+        color: "#000000"
+    },
+    {
+        title: "Vercel",
+        icon: Icons.SiVercel,
+        color: "#000000"
+    },
+    {
+        title: "Selenium",
+        icon: Icons.SiSelenium,
+        color: "#43B02A"
+    },
+    {
+        title: "Linux",
+        icon: Icons.SiLinux,
+        color: "#FCC624"
+    },
+    {
+        title: "Git",
+        icon: Icons.SiGit,
+        color: "#F05032"
+    }
+
+]
+
+export default TECHNOLOGIES;
