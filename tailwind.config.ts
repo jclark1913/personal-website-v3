8e98a5f1570afa3d7a78ad80559553510469c1d9
Justin Clark <jclarksummit@gmail.com>
2024-02-17 11:15:02 -0500

    Add custom utility classes for light/dark mode (unfin)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,6 +9,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        sitebackground: "rgba(var(--main-background))",
+        headertext: "rgba(var(--header-text))",
+        paragraphtext: "rgba(var(--paragraph-text))",
+        experiencetab: "rgba(var(--experience-tab))",
+        experiencetabactive: "rgba(var(--experience-tab-active))",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
