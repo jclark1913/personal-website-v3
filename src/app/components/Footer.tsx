7cc051927a6d3e204862735af768bafdb46c15d0
Justin Clark <jclarksummit@gmail.com>
2024-02-18 13:54:41 -0500

    Tweak colors/formatting

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,7 +7,6 @@ const Footer = () => {
     <footer className="border z-10 border-t-normalborder border-l-transparent border-r-transparent border-b-transparent flex align-middle justify-center text-white">
       <div className="container p-12 flex flex-col text-center align-middle justify-center text-footertext">
         <small className="">Justin Clark © {currYear}</small>
-        <small className="">This isn't my first website. Check out <a href="">v1</a> and <a href="">v2</a>.</small>
       </div>
     </footer>
   );
