b9c29880fc13a28f738dc0a5285cc46244ebbc20
Justin Clark <jclarksummit@gmail.com>
2024-02-18 13:53:32 -0500

    Tweak text + add socials

--- src/app/components/ContactSection.tsx
+++ src/app/components/ContactSection.tsx
@@ -1,27 +1,47 @@
 import React from "react";
+import { SOCIALS } from "../lib/Socials";
 
 const ContactSection = () => {
   return (
-    <div id="contact" className="my-24">
-      <h1 className="text-4xl font-bold text-headertext mb-4 text-center">
-        Contact
+    <div id="contact" className="lg:mx-24 mt-24 flex flex-col">
+      <h1 className="border-t-2 border-normalborder pt-24 text-4xl font-bold text-headertext mb-4 text-center">
+        Contact Me
       </h1>
-      <div className="grid grid-cols-2 gap-8 text-paragraphtext">
+      <div className="flex flex-col sm:flex-row place-items-stretch gap-8 text-paragraphtext">
         <div className="flex flex-col gap-4">
-          <h2 className="text-left text-md sm:text-lg">Let's Connect</h2>
-          <p className="">
-            I'm currently looking for new opportunities and open to new roles.
-            My inbox is open - send me a message if you'd like to ask a
-            question, connect or get in touch for any reason.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4">
-          <h2 className="text-left text-md sm:text-lg">
-            This is here to take up space
+          <h2 className="text-left text-md font-extrabold sm:text-3xl">
+            Let's Connect:
           </h2>
-          <p className="">Dont know what will go here honestly</p>
+          <div className="flex flex-col gap-3 sm:w-full text-justify">
+            <p className="sm:text-2xl">
+              I'm currently looking for new opportunities and open to new roles.
+            </p>
+            <p className="">
+              My inbox is open - send me a message if you'd like to ask a
+              question, connect or set up a time to chat.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-row gap-3 justify-center sm:grid sm:grid-cols-2 sm:w-2/3 items-center sm:ml-24 p-8">
+          {SOCIALS.map((social, index) => (
+            <a
+              key={index}
+              href={social.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-row justify-start items-center gap-4 text-experiencetabinactive hover:text-headertext"
+            >
+              <social.icon className="size-8" />
+              <p className="hidden xl:flex text-md xl:text-2xl">
+                {social.title}
+              </p>
+            </a>
+          ))}
         </div>
       </div>
+      <p className="text-center text-xl text-paragraphtext my-12">
+        Thanks for visiting!
+      </p>
     </div>
   );
 };
