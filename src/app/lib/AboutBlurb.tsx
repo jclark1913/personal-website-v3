a6ee71aca3097398bc9ade2dc7f24cdef0e4f093
Justin Clark <jclarksummit@gmail.com>
2024-02-18 00:01:43 -0500

    Move text from about me into const in /lib

---
+++ src/app/lib/AboutBlurb.tsx
@@ -0,0 +1,32 @@
+export const ABOUT_BLURB = (
+  <>
+    <p className="mb-2 ">
+      I'm a full stack software developer and technical consultant based in
+      Michigan. Prior to moving back to my home state in 2022, I lived for more
+      than seven years in the Middle East where I worked in journalism, analysis
+      and translation.
+    </p>
+
+    <p className="mb-2">
+      Today, most of my work is focused on developing software aimed at boosting
+      the capacity of researchers and analysts. I'm also on the lookout for
+      full-time software engineering roles.
+    </p>
+
+    <p className="mb-2">
+      {" "}
+      Some of my recent open source projects include a{" "}
+      <a href="https://github.com/bellingcat/wayback-google-analytics">
+        tool
+      </a>{" "}
+      for identifying disinformation networks (read my Bellingcat article{" "}
+      <a href="https://www.bellingcat.com/resources/2024/01/09/using-the-wayback-machine-and-google-analytics-to-uncover-disinformation-networks/">
+        here
+      </a>
+      ) and an <a href="#syria-daily-brief">AI-powered web scraper</a> to
+      monitor Syrian media. But I tinker a lot, so follow me on{" "}
+      <a href="https://github.com/jclark1913">github</a> to see what I'm working
+      on.
+    </p>
+  </>
+);
