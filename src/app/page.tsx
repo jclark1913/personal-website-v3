9193a26ba6b7a11f63e4495f534988eafe0fa673
Justin Clark <jclarksummit@gmail.com>
2024-02-18 00:02:14 -0500

    Add theme context provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,24 @@ import NavBar from "./components/NavBar";
 import AboutSection from "./components/AboutSection";
 import SkillsSection from "./components/SkillsSection";
 import ProjectSection from "./components/ProjectSection";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
+import { ThemeProvider, ThemeContext } from "./components/ThemeContext";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#161b22]">
-      <NavBar />
-      <div className=" container mx-auto px-12 p-4 mt-24">
-        <HeroSection />
-        <AboutSection />
-        <SkillsSection />
-        <ProjectSection />
-      </div>
-    </main>
+    <ThemeProvider>
+      <main className="flex min-h-screen flex-col bg-sitebackground">
+        <NavBar />
+        <div className=" container mx-auto px-12 p-4 mt-24">
+          <HeroSection />
+          <AboutSection />
+          <SkillsSection />
+          <ProjectSection />
+          <ContactSection />
+        </div>
+        <Footer />
+      </main>
+    </ThemeProvider>
   );
 }
