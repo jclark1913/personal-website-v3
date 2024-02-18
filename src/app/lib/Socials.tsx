f1a307e35fa4c752a94be4079cb29ad03a63ee46
Justin Clark <jclarksummit@gmail.com>
2024-02-18 00:40:13 -0500

    initial commit

---
+++ src/app/lib/Socials.tsx
@@ -0,0 +1,24 @@
+import * as Icons from "react-icons/si";
+
+export const SOCIALS = [
+  {
+    title: "Github",
+    icon: Icons.SiGithub,
+    link: "https://github.com/jclark1913",
+  },
+  {
+    title: "LinkedIn",
+    icon: Icons.SiLinkedin,
+    link: "https://www.linkedin.com/in/justin-w-clark/",
+  },
+  {
+    title: "Twitter",
+    icon: Icons.SiTwitter,
+    link: "https://twitter.com/justinclarkjo",
+  },
+  {
+    title: "Email",
+    icon: Icons.SiGmail,
+    link: "mailto: jclarksummit@gmail.com",
+  },
+];
