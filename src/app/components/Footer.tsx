17f48180e9865bb07f56767b7210091b1f6db4e4
Justin Clark <jclarksummit@gmail.com>
2024-02-18 00:39:16 -0500

    Tweak border + add footer color class

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,10 +4,10 @@ import React from "react";
 const Footer = () => {
   const currYear = new Date().getFullYear();
   return (
-    <footer className="border z-10 border-t-slate-400 border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
-        <span>{"</>"}</span>
-        <small className="text-slate-400">Justin Clark © {currYear}</small>
+    <footer className="border z-10 border-t-normalborder border-l-transparent border-r-transparent border-b-transparent flex align-middle justify-center text-white">
+      <div className="container p-12 flex flex-col text-center align-middle justify-center text-footertext">
+        <small className="">Justin Clark © {currYear}</small>
+        <small className="">This isn't my first website. Check out <a href="">v1</a> and <a href="">v2</a>.</small>
       </div>
     </footer>
   );
